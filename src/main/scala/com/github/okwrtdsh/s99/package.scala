cee54f1bdb38a71ed6469c791eb33af4a482f98c
okwrtdsh <okwrtdsh@gmail.com>
2018-11-17 00:44:32 +0900

    apply scalafmt and add P01-P05

--- src/main/scala/com/github/okwrtdsh/s99/package.scala
+++ src/main/scala/com/github/okwrtdsh/s99/package.scala
@@ -1,9 +1,59 @@
 package com.github.okwrtdsh
 
 package object S99 {
+
   /** P01 */
   def last[T](list: List[T]) = list.last
 
+  def last2[T](list: List[T]): T = list match {
+    case Nil         => ???
+    case head :: Nil => head
+    case _ :: tail   => last2(tail)
+  }
+
+  def last3[T](list: List[T]): T = list.reduceLeft { (_, e) =>
+    e
+  }
+
   /** P02 */
   def penultimate[T](list: List[T]) = list.init.last
+
+  def penultimate2[T](list: List[T]): T = list match {
+    case Nil              => ???
+    case _ :: Nil         => ???
+    case head :: _ :: Nil => head
+    case _ :: tail        => penultimate2(tail)
+  }
+
+  /** P03 */
+  def nth[T](n: Int, list: List[T]) = list(n)
+
+  def nth2[T](n: Int, list: List[T]): T = list match {
+    case head :: _ if n == 0 => head
+    case head :: tail        => nth2(n - 1, tail)
+  }
+
+  /** P04 */
+  def length1[T](list: List[T]) = list.length
+
+  def length2[T](list: List[T]): Int = list match {
+    case Nil       => 0
+    case _ :: tail => 1 + length2(tail)
+  }
+
+  def length3[T](list: List[T]) = list.foldLeft(0) { (c, _) =>
+    c + 1
+  }
+
+  /** P05 */
+  def reverse[T](list: List[T]) = list.reverse
+
+  def reverse2[T](list: List[T]): List[T] = list match {
+    case Nil          => Nil
+    case head :: tail => reverse2(tail) :+ head
+  }
+
+  def reverse3[T](list: List[T]) = list.foldLeft(List[T]()) { (l, e) =>
+    e +: l
+  }
 }
--- src/test/scala/com/github/okwrtdsh/s99/S99.scala
+++ src/test/scala/com/github/okwrtdsh/s99/S99.scala
@@ -6,8 +6,44 @@ class S99Spec extends FlatSpec with Matchers {
   "P01" should "last" in {
     last(List(1, 1, 2, 3, 5, 8)) should be(8)
   }
+  it should "last2" in {
+    last2(List(1, 1, 2, 3, 5, 8)) should be(8)
+  }
+  it should "last3" in {
+    last3(List(1, 1, 2, 3, 5, 8)) should be(8)
+  }
 
   "P02" should "penultimate" in {
     penultimate(List(1, 1, 2, 3, 5, 8)) should be(5)
+  }
+  it should "penultimate2" in {
+    penultimate2(List(1, 1, 2, 3, 5, 8)) should be(5)
+  }
+
+  "P03" should "nth" in {
+    nth(2, List(1, 1, 2, 3, 5, 8)) should be(2)
+  }
+  it should "nth2" in {
+    nth2(2, List(1, 1, 2, 3, 5, 8)) should be(2)
+  }
+
+  "P04" should "length1" in {
+    length1(List(1, 1, 2, 3, 5, 8)) should be(6)
+  }
+  it should "length2" in {
+    length2(List(1, 1, 2, 3, 5, 8)) should be(6)
+  }
+  it should "length3" in {
+    length3(List(1, 1, 2, 3, 5, 8)) should be(6)
+  }
+
+  "P05" should "reverse" in {
+    reverse(List(1, 1, 2, 3, 5, 8)) should be(List(8, 5, 3, 2, 1, 1))
+  }
+  it should "reverse2" in {
+    reverse2(List(1, 1, 2, 3, 5, 8)) should be(List(8, 5, 3, 2, 1, 1))
+  }
+  it should "reverse3" in {
+    reverse3(List(1, 1, 2, 3, 5, 8)) should be(List(8, 5, 3, 2, 1, 1))
   }
 }
